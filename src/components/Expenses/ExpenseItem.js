5601f6345926c58fa180863168a016a24ff46484
AmirAsadzadeh <amirasadzadeh2021@gmail.com>
2022-05-05 17:26:30 +0430

    components organized

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,7 +1,7 @@
 import "./ExpenseItem.css";
 import React from "react";
 import ExpenseDate from "./ExpenseDate.js";
-import Card from "./Card";
+import Card from "../UI/Card";
 
 function ExpenseItem(props) {
   return (
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,7 +1,7 @@
 import "./Expenses.css";
 import React from "react";
 import ExpenseItem from "./ExpenseItem";
-import Card from "./Card";
+import Card from "../UI/Card";
 
 function Expenses() {
   const expenses = [
